9ff48920c040d517ce624038498d07ae233ec50e
abdelrahman <60198078+Badr-0101@users.noreply.github.com>
2026-01-16 00:41:34 +0200

    Remove basename from BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { AuthProvider } from './context/AuthContext'
 import {QueryProvider} from "@/lib/react-query/QueryProvider"
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/snapgram">
+    <BrowserRouter >
       <QueryProvider>
         <AuthProvider>
         <App />
