bb80fb150965db7e65284686f99d30c45a076462
abdelrahman <60198078+Badr-0101@users.noreply.github.com>
2026-04-07 00:52:08 +0200

    Update image source in AuthLayout component

--- src/auth/AuthLayout.tsx
+++ src/auth/AuthLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet, Navigate } from "react-router-dom"
-
+import sideImg from "@/assets/images/side-img.svg"
 const AuthLayout = () => {
    const isAuth = true
     
@@ -9,7 +9,7 @@ const AuthLayout = () => {
                 <section className="flex flex-1 h-screen justify-center items-center flex-col ">
                     <Outlet />
                 </section>
-                <img src="/public/assets/images/side-img.svg"
+                <img src={sideImg}
                             className={"hidden h-screen  xl:block w-1/2 object-cover bg-no-repeat"} />
                 </div>
     )
